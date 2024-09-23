6915829928669782a0585ce9606535cb3e27bfd8
dborgess <daviborges2014@gmail.com>
2024-09-23 00:05:17 -0300

    style: :art: Largura do modal

--- protected/views/site/upload.php
+++ protected/views/site/upload.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload de Arquivo CSV</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="/css/style.css">     
 </head>
 <body>
 
