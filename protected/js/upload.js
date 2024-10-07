b6c0b7cb4f319470d27a8108a9785ab0c5ca6414
dborgess <daviborges2014@gmail.com>
2024-10-07 15:18:39 -0300

    feat: :zap: accesToken agora é armazenado temporáriamente na api localStorage

--- protected/js/upload.js
+++ protected/js/upload.js
@@ -9,7 +9,6 @@ function formatDate(dateString) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
     document.getElementById('uploadForm').onsubmit = async function(event) {
         event.preventDefault();
 
@@ -62,129 +61,133 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     async function authenticateAndRegister(data) {
-        try {
-            const authData = await Api.authenticate();
+        let authData;
+        const storedToken = localStorage.getItem('accessToken'); // Busca o token armazenado
+
+        if (storedToken) {
+            authData = { accessToken: storedToken }; // Usa o token armazenado
+        } else {
+            authData = await Api.authenticate();
             if (authData.accessToken) {
-                const successResults = [];
-                const errorResults = [];
-
-                const totalClients = data.length;
-
-                showProgressBar(); // Exibir a barra de progresso ao iniciar o cadastro
-
-                const registerPromises = data.map(async (cliente, index) => {
-                    const maritalStatusMap = {
-                        'married': 'C',
-                        'single': 'S',
-                        'divorced': 'D',
-                        'widowed': 'V',
-                        'not informed': null // 'null' será enviado como valor nulo
-                    };
-
-                    // Cria um objeto com os dados do cliente
-                    let clienteObj = {
-                        companyId: parseInt(cliente[0]) || null,
-                        name: cliente[2] || "Empresa Fake ABC Ltda2",
-                        tradeName: cliente[3] || "",
-                        pronunciation: cliente[4] || null,
-                        fieldOfActivity: cliente[40] || "Indústria",
-                        profession: cliente[46] || null,
-                        notes: cliente[41] || "Uma empresa que produz droides de batalha, incluindo os Droidekas",
-                        cellNumber: cliente[13] || "11988997766",
-                        website: cliente[39] || "fakeabc.app",
-                        hasLoginAccess: false,
-                        automaticallyIssueNfse: "notIssue",
-                        notesNfse: cliente[42] || "Teste observações na NFSe",
-                        hasIssRetention: true,
-                        address: {
-                            zipCode: cliente[21] || "13058-111",
-                            state: cliente[23] || "SP",
-                            city: cliente[22] || "Campinas",
-                            street: cliente[17] || "Rua Alziro Arten",
-                            number: cliente[18] || "443",
-                            neighborhood: cliente[20] || "Conjunto Habitacional Parque da Floresta",
-                            additionalDetails: cliente[19] || "Sala 4, Térreo"
-                        },
-                        phones: cliente[12] ? cliente[12].split(';').map(phone => phone.trim()) : [],
-                        emailsMessage: cliente[11] ? cliente[11].split(';').map(email => email.trim()) : [],
-                        emailsFinancialMessages: cliente[10] ? cliente[10].split(';').map(email => email.trim()) : [],
-                        isNetworkingProfileVisible: true,
-                        isBlockedBookingCustomerArea: true,
-                        isAllowedBookingOutsideBusinessHours: false,
-                        internetPlan: "Plano 20MB",
-                        businessPresentation: cliente[42] || "Breve apresentação da empresa",
-                        offeredServicesProducts: cliente[43] || "Serviços e produtos oferecidos",
-                        receptionOrientations: cliente[45] || "Orientações de atendimento",
-                        mailingOrientations: "Orientações de correspondência",
-                        mailingAddress: {
-                            zipCode: cliente[29] || "13060-008",
-                            state: cliente[31] || "SP",
-                            city: cliente[30] || "Campinas",
-                            street: cliente[25] || "Rua Antônio Menito",
-                            number: cliente[26] || "264",
-                            neighborhood: cliente[28] || "Jardim Anchieta",
-                            additionalDetails: cliente[27] || "Bloco A1"
-                        },
-                        extensionNumbers: []
-                    };
-
-                    // Verifica se o cliente é pessoa jurídica (CNPJ presente) ou física (CPF)
-                    if (cliente[6]) { // Se CNPJ está presente
-                        clienteObj.legalPerson = {
-                            cnpj: cliente[6],
-                            foundationDate: cliente[5] ? formatDate(cliente[5]) : null,
-                            municipalInscription: cliente[42] || null, // Preencha conforme necessário
-                            stateInscription: cliente[41] || null // Preencha conforme necessário
-                        };
-                    } else {
-                        // Ajuste na data de nascimento
-                        const birthDate = cliente[9];
-                        clienteObj.naturalPerson = {
-                            cpf: cliente[7] || null,
-                            rg: cliente[8] || null,
-                            birthDate: birthDate ? formatDate(birthDate) : null, // Formata a data corretamente
-                            issuingAuthority: cliente[10] || null,
-                            maritalStatus: maritalStatusMap[cliente[47]] || null // Mapeia o estado civil
-                        };
-                    }
-
-                    try {
-                        const registerData = await Api.registerCustomer(clienteObj, authData.accessToken);
-                        if (registerData.success) {
-                            successResults.push({ id: registerData.id, name: clienteObj.name });
-                        } else {
-                            throw new Error(registerData.error || 'Erro ao cadastrar cliente');
-                        }
-                    } catch (error) {
-                        errorResults.push({ name: clienteObj.name, error: error.message });
-                    }
-
-                    // Atualiza a barra de progresso após cada cadastro
-                    const percentage = Math.round(((index + 1) / totalClients) * 100);
-                    updateProgressBar(percentage);
-                });
-
-                await Promise.all(registerPromises);
-                hideProgressBar(); // Oculta a barra de progresso ao final do cadastro
-
-                // Aqui você deve exibir a tela de sucesso ou relatório
-                if (typeof UI !== 'undefined') {
-                    UI.displayRegistrationReport(successResults, errorResults); // Exibe o relatório de cadastro
-                }
-
-                // Exibir modal de sucesso
-                $('#successModal').modal('show'); // Mostra o modal de sucesso
-
+                localStorage.setItem('accessToken', authData.accessToken); // Armazena o novo token
             } else {
                 throw new Error(authData.error || 'Erro ao autenticar');
             }
-        } catch (error) {
-            console.error('Erro durante o registro:', error);
-            hideProgressBar(); // Oculta a barra de progresso em caso de erro
-            if (typeof UI !== 'undefined') {
-                UI.showError('Ocorreu um erro durante o registro. Por favor, tente novamente.');
-            }
         }
+
+        const successResults = [];
+        const errorResults = [];
+        const totalClients = data.length;
+
+        showProgressBar(); // Exibir a barra de progresso ao iniciar o cadastro
+
+        const registerPromises = data.map(async (cliente, index) => {
+            const maritalStatusMap = {
+                'married': 'C',
+                'single': 'S',
+                'divorced': 'D',
+                'widowed': 'V',
+                'not informed': null // 'null' será enviado como valor nulo
+            };
+
+            // Cria um objeto com os dados do cliente
+            let clienteObj = {
+                companyId: parseInt(cliente[0]) || null,
+                name: cliente[2] || "Empresa Fake ABC Ltda2",
+                tradeName: cliente[3] || "",
+                pronunciation: cliente[4] || null,
+                fieldOfActivity: cliente[40] || "Indústria",
+                profession: cliente[46] || null,
+                notes: cliente[41] || "Uma empresa que produz droides de batalha, incluindo os Droidekas",
+                cellNumber: cliente[13] || "11988997766",
+                website: cliente[39] || "fakeabc.app",
+                hasLoginAccess: false,
+                automaticallyIssueNfse: "notIssue",
+                notesNfse: cliente[42] || "Teste observações na NFSe",
+                hasIssRetention: true,
+                address: {
+                    zipCode: cliente[21] || "13058-111",
+                    state: cliente[23] || "SP",
+                    city: cliente[22] || "Campinas",
+                    street: cliente[17] || "Rua Alziro Arten",
+                    number: cliente[18] || "443",
+                    neighborhood: cliente[20] || "Conjunto Habitacional Parque da Floresta",
+                    additionalDetails: cliente[19] || "Sala 4, Térreo"
+                },
+                phones: cliente[12] ? cliente[12].split(';').map(phone => phone.trim()) : [],
+                emailsMessage: cliente[11] ? cliente[11].split(';').map(email => email.trim()) : [],
+                emailsFinancialMessages: cliente[10] ? cliente[10].split(';').map(email => email.trim()) : [],
+                isNetworkingProfileVisible: true,
+                isBlockedBookingCustomerArea: true,
+                isAllowedBookingOutsideBusinessHours: false,
+                internetPlan: "Plano 20MB",
+                businessPresentation: cliente[42] || "Breve apresentação da empresa",
+                offeredServicesProducts: cliente[43] || "Serviços e produtos oferecidos",
+                receptionOrientations: cliente[45] || "Orientações de atendimento",
+                mailingOrientations: "Orientações de correspondência",
+                mailingAddress: {
+                    zipCode: cliente[29] || "13060-008",
+                    state: cliente[31] || "SP",
+                    city: cliente[30] || "Campinas",
+                    street: cliente[25] || "Rua Antônio Menito",
+                    number: cliente[26] || "264",
+                    neighborhood: cliente[28] || "Jardim Anchieta",
+                    additionalDetails: cliente[27] || "Bloco A1"
+                },
+                extensionNumbers: []
+            };
+
+            // Verifica se o cliente é pessoa jurídica (CNPJ presente) ou física (CPF)
+            if (cliente[6]) { // Se CNPJ está presente
+                clienteObj.legalPerson = {
+                    cnpj: cliente[6],
+                    foundationDate: cliente[5] ? formatDate(cliente[5]) : null,
+                    municipalInscription: cliente[42] || null, // Preencha conforme necessário
+                    stateInscription: cliente[41] || null // Preencha conforme necessário
+                };
+            } else {
+                // Ajuste na data de nascimento
+                const birthDate = cliente[9];
+                clienteObj.naturalPerson = {
+                    cpf: cliente[7] || null,
+                    rg: cliente[8] || null,
+                    birthDate: birthDate ? formatDate(birthDate) : null, // Formata a data corretamente
+                    issuingAuthority: cliente[10] || null,
+                    maritalStatus: maritalStatusMap[cliente[47]] || null // Mapeia o estado civil
+                };
+            }
+
+            try {
+                const registerData = await Api.registerCustomer(clienteObj, authData.accessToken);
+                if (registerData.success) {
+                    successResults.push({ id: registerData.id, name: clienteObj.name });
+                } else {
+                    throw new Error(registerData.error || 'Erro ao cadastrar cliente');
+                }
+            } catch (error) {
+                if (error.message.includes('400')) {
+                    // Exiba um erro específico para validação
+                    errorResults.push({ name: clienteObj.name, error: 'Erro de validação: ' + error.message });
+                } else {
+                    errorResults.push({ name: clienteObj.name, error: 'Erro inesperado: ' + error.message });
+                }
+            }
+
+            // Atualiza a barra de progresso após cada cadastro
+            const percentage = Math.round(((index + 1) / totalClients) * 100);
+            updateProgressBar(percentage);
+        });
+
+        await Promise.all(registerPromises);
+        hideProgressBar(); // Oculta a barra de progresso ao final do cadastro
+
+        // Aqui você deve exibir a tela de sucesso ou relatório
+        if (typeof UI !== 'undefined') {
+            UI.displayRegistrationReport(successResults, errorResults); // Exibe o relatório de cadastro
+        }
+
+        // Exibir modal de sucesso
+        $('#successModal').modal('show'); // Mostra o modal de sucesso
     }
 });
 
